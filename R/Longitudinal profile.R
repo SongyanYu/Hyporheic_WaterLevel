6cf16aff3f8a86c8996246ba87fa08a9edec536e
SongyanYu <songyan.yu@griffithuni.edu.au>
2020-06-04 11:34:45 +1000

    reorder script line number

--- R/Longitudinal profile.R
+++ R/Longitudinal profile.R
@@ -1,26 +1,25 @@
-##This script is used to plot longitudinal profile.
-##This is based on Kobble creel 1m DEM data
+#---
+# This script is used to plot longitudinal profile for Kobble creek 1m DEM data
+# Author: Songyan Yu
+# Date created: 04/06/2020
+#---
+
+setwd("../../Kobble Cr")
 
 library(raster)
-setwd("H:/My Drive/PhD at GU/Part 3 River channel identification/Kobble Cr")
 
 longprofile<-raster("kob_longprof.tif")  #1m longitudinal profile
-
-flowdir<-raster("Flowdir_kob.tif")
-
 plot(values(longprofile)[!is.na(values(longprofile))])
-
-value<-values(longprofile)[!is.na(values(longprofile))] #the order of "value" is from top to bottom
-flowdir.value<-values(flowdir)[!is.na(values(flowdir))]
-
+value<-values(longprofile)[!is.na(values(longprofile))] # the order of "value" is from top to bottom
 value.location<-which(!is.na(values(longprofile)))
-
 value.col<-value.location%%4708  #4708 is the number of columns of the raster
 value.row<-value.location%/%4708
 
-## Rearrange all of the main stem pixels in the order of upstream to downstream.
-##They were initially in a scanning order (from top to bottom and from left to right)
+flowdir<-raster("Flowdir_kob.tif")
+flowdir.value<-values(flowdir)[!is.na(values(flowdir))]
 
+## Rearrange all of the main stem pixels in the order of upstream to downstream.
+## They were initially in a scanning order (from top to bottom and from left to right)
 pool<-data.frame(x=value.col,y=value.row)
 
 # Starting from the most upstream cell, which has the biggest row number.
@@ -57,10 +56,8 @@ for(i in 1:(length(flowdir.value)-1)){
 }
 
 ##plot the longitudinal channel profile
-
 value<-value[rev(flowdirection)]
 plot(value)
-
 
 width<-7
 asp<-1.5
