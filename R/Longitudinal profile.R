9e9ecef5bc930b009cdb7c47e8696dedf60fc78b
SongyanYu <songyan.yu@griffithuni.edu.au>
2020-06-04 11:47:15 +1000

    cancel plotting to save time

--- R/Longitudinal profile.R
+++ R/Longitudinal profile.R
@@ -6,7 +6,7 @@
 library(raster)
 
 longprofile<-raster("../../Kobble Cr/kob_longprof.tif")  #1m longitudinal profile
-plot(values(longprofile)[!is.na(values(longprofile))])
+#plot(values(longprofile)[!is.na(values(longprofile))])
 value<-values(longprofile)[!is.na(values(longprofile))] # the order of "value" is from top to bottom
 value.location<-which(!is.na(values(longprofile)))
 value.col<-value.location%%4708  #4708 is the number of columns of the raster
