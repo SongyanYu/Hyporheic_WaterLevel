afe63e2b0362ac39a5763e56b1a2814777a56594
SongyanYu <songyan.yu@griffithuni.edu.au>
2020-06-04 12:35:28 +1000

    better description

--- R/IdentifyWaterPools.R
+++ R/IdentifyWaterPools.R
@@ -9,7 +9,7 @@
 source("R/Longitudinal profile.R")  # need the "value" variable
 summary(value)
 
-# customised funciton
+# customised funciton ro calculate local maxima
 localMaxima <- function(x) {
   # Use -Inf instead if x is numeric (non-integer)
   y <- diff(c(-.Machine$integer.max, x)) > 0L
@@ -25,7 +25,6 @@ localMaxima <- function(x) {
 local.maxima.location<-localMaxima(value) #local maxima 
 
 #start from the first maxima (the downstream end)
-
 start.point<-local.maxima.location[1]
 i<-1   #the number of identified water pools
 pool.points.list<-list()   #the extent of each water pool
@@ -33,7 +32,6 @@ pool.points.list<-list()   #the extent of each water pool
 while(start.point<max(local.maxima.location)){
   
   pool.point<-start.point
-  
   j<-1
   pool.point.c<-c()
   
@@ -49,12 +47,9 @@ while(start.point<max(local.maxima.location)){
     pool.point.c[j+1]<-start.point
   }
   
-  
   pool.points.list[[i]]<-pool.point.c
   i<-i+1
-  
   end.point<-pool.point
-  
   start.point<-local.maxima.location[local.maxima.location>=end.point][1]
 }
 
@@ -79,37 +74,28 @@ i=2
 while(local.maxima.location[downstream.end.pool]<max(local.maxima.location)){
   
   top.elevation.pool<-value[local.maxima.location[downstream.end.pool]]
-  
   start.pool.location[i]<-local.maxima.location[downstream.end.pool]
   start.pool.elevation[i]<-top.elevation.pool
   
   upstream.end.pool<-which(value[local.maxima.location[downstream.end.pool]:length(value)]>top.elevation.pool)[1]+local.maxima.location[downstream.end.pool]-1
-  
   end.pool.location[i]<-upstream.end.pool
-  
   downstream.end.pool<-which(local.maxima.location>=upstream.end.pool)[1]
   
   i=i+1
 }
 
 ##Identify surface water availability (i.e. remaining water pools) when river channel becomes disconnected.
-
-#pool depth
-
+#pool depth and width
 pool.depth<-c()
 pool.width<-c()
 
 for(i in 1:length(start.pool.location)){
-  
   lowest.ele.in.pool<-min(value[start.pool.location[i]:end.pool.location[i]])
   pool.depth[i]<-start.pool.elevation[i]-lowest.ele.in.pool
-  
   pool.width[i]<-end.pool.location[i]-start.pool.location[i]
-  
 }
 
 # exclude identified pools that are less than 0.1m deep.
-
 exclu.pool<-which(pool.depth<=0.1)  #0.1m is a number referred to Zimmermann's paper (2008)
 start.pool.location<-start.pool.location[-exclu.pool]
 end.pool.location<-end.pool.location[-exclu.pool]
@@ -122,52 +108,26 @@ for(i in exclu.pool){
 }
 
 #Integrate all water pools to estimate surface water extent (% length of river channel covered by surface water)
-
+sum(pool.width)/length(value)
 
 ## plot for Kobble Cr
 
 width<-5
 asp<-2.5
 ppi<-100
-
-png(paste0("Longitudinal profile Kobble Cr_1.png"),width = width*asp*ppi,height = width*ppi,res=ppi)
-
-plot(value[2925:3255]~c(2925:3255),type="l",xlab=c("Upstream distance /m"),ylab=c("Elevation /m"),ylim=c(80,86))
-
-for(i in 1:length(pool.points.list)){
-  
-  temp_df<-data.frame(x=pool.points.list[[i]],y=value[pool.points.list[[i]]])
-  
-  end=nrow(temp_df)
-  b=temp_df[1,2]
-  
-  a=temp_df[(end-2),1]+(b-temp_df[(end-2),2])*(temp_df[(end-1),1]-temp_df[(end-2),1])/(temp_df[(end-1),2]-temp_df[(end-2),2])
-  newrow<-c(a,b)
-  
-  temp_df[(end-1),]<-newrow
-  
-  polygon(temp_df,col="blue")
-}
-
-dev.off()
-
-
 png(paste0("Longitudinal profile Kobble Cr_2.png"),width = width*asp*ppi,height = width*ppi,res=ppi)
 
-plot(value[3245:3475]~c(3245:3475),type="l",xlab=c("Upstream distance /m"),ylab=c("Elevation /m"),ylim=c(83,88))
+plot(value[2500:4067]~c(2500:4067),type="l",xlab=c("Upstream distance /m"),ylab=c("Elevation /m"),ylim=c(75,93),xlim=c(2500,4067))
 
 for(i in 1:length(pool.points.list)){
-  
   temp_df<-data.frame(x=pool.points.list[[i]],y=value[pool.points.list[[i]]])
   
   end=nrow(temp_df)
   b=temp_df[1,2]
-  
   a=temp_df[(end-2),1]+(b-temp_df[(end-2),2])*(temp_df[(end-1),1]-temp_df[(end-2),1])/(temp_df[(end-1),2]-temp_df[(end-2),2])
   newrow<-c(a,b)
   
   temp_df[(end-1),]<-newrow
-  
   polygon(temp_df,col="blue")
 }
 
